26326322f5a95db6195f0965d4c4628112233573
ningxianseng <ng14418hqi02@126.com>
2024-05-19 17:02:30 +0800

    a

--- src/PLANE_ESP8266_Brushless2S/PLANE_ESP8266_Brushless2S.ino
+++ src/PLANE_ESP8266_Brushless2S/PLANE_ESP8266_Brushless2S.ino
@@ -14,10 +14,10 @@
 #define PITCH_PIN 15
 #define ROLL_PIN 13
 
-#define INTERRUPT_PIN 0  // use pin 2 on Arduino Uno & most boards
-#define LED_PIN 2 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+#define INTERRUPT_PIN 0 // use pin 2 on Arduino Uno & most boards
+#define LED_PIN 2       // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 
-String system_info_file = "/system_info.json"; 
+String system_info_file = "/system_info.json";
 
 uint8_t broadcast_address[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 uint8_t rc_address[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
@@ -57,10 +57,11 @@ VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measure
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
+uint8_t teapotPacket[14] = {'$', 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0x00, 0x00, '\r', '\n'};
+volatile bool mpuInterrupt = false; // indicates whether MPU interrupt pin has gone high
+void dmpDataReady()
+{
+  mpuInterrupt = true;
 }
 
 double kp = 4;
@@ -68,7 +69,7 @@ double ki = 4;
 double kd = 0.25;
 double roll_power_out = 0;
 
-PID rollPowerPID(&mpu_roll, &roll_power_out, &roll,kp,ki,kd, DIRECT);
+PID rollPowerPID(&mpu_roll, &roll_power_out, &roll, kp, ki, kd, DIRECT);
 
 Servo engine_left;
 Servo engine_right;
@@ -76,7 +77,8 @@ Servo servo_yaw;
 Servo servo_pitch;
 Servo servo_roll;
 
-void save_cofig(){
+void save_cofig()
+{
   DynamicJsonDocument system_info(512);
   system_info["kp"] = kp;
   system_info["ki"] = ki;
@@ -88,31 +90,38 @@ void save_cofig(){
   String system_info_str;
   serializeJson(system_info, system_info_str);
   Serial.println(system_info_str);
-  File file = SPIFFS.open(system_info_file,"w");
+  File file = SPIFFS.open(system_info_file, "w");
   serializeJson(system_info, file);
   file.close();
 }
 
-void OnDataReceived(uint8_t *macAddr, uint8_t *data, uint8_t dataLength) {
+void OnDataReceived(uint8_t *macAddr, uint8_t *data, uint8_t dataLength)
+{
   String receivedText;
-  char text[dataLength+1];
+  char text[dataLength + 1];
   memcpy(text, (char *)data, dataLength);
   text[dataLength] = '\0';
   receivedText = String(text);
-  //Serial.println(receivedText);
+  // Serial.println(receivedText);
   DynamicJsonDocument order_data(512);
   deserializeJson(order_data, receivedText);
-  if(order_data["from"] == "rc"){
-    if(areMacAddressesEqual(rc_address,broadcast_address)){
-      if(order_data["request"] == "matching_codes"){
-        for(int i = 0;i < 6 ; i++){
+  if (order_data["from"] == "rc")
+  {
+    if (areMacAddressesEqual(rc_address, broadcast_address))
+    {
+      if (order_data["request"] == "matching_codes")
+      {
+        for (int i = 0; i < 6; i++)
+        {
           rc_address[i] = macAddr[i];
         }
         Serial.println("RC MATCHING CODES");
       }
     }
-    if(areMacAddressesEqual(rc_address,macAddr)){
-      if(order_data["request"] == "flight_order"){
+    if (areMacAddressesEqual(rc_address, macAddr))
+    {
+      if (order_data["request"] == "flight_order")
+      {
         yaw = order_data["yaw"];
         power = order_data["power"];
         diff_power = order_data["diff_power"];
@@ -122,67 +131,83 @@ void OnDataReceived(uint8_t *macAddr, uint8_t *data, uint8_t dataLength) {
         flap_angle = order_data["flap_angle"];
         landing_gear = order_data["landing_gear"];
         led = order_data["led"];
-      }else{
+      }
+      else
+      {
         Serial.println(receivedText);
       }
-      if(order_data["request"] == "yaw_left"){
+      if (order_data["request"] == "yaw_left")
+      {
         yaw_correct = yaw_correct - 1;
         save_cofig();
       }
-      if(order_data["request"] == "yaw_right"){
+      if (order_data["request"] == "yaw_right")
+      {
         yaw_correct = yaw_correct + 1;
         save_cofig();
       }
-      if(order_data["request"] == "twin_engine_left"){
+      if (order_data["request"] == "twin_engine_left")
+      {
         twin_engine_power_correct = twin_engine_power_correct - 1;
         save_cofig();
       }
-      if(order_data["request"] == "twin_engine_right"){
+      if (order_data["request"] == "twin_engine_right")
+      {
         twin_engine_power_correct = twin_engine_power_correct + 1;
         save_cofig();
       }
-      if(order_data["request"] == "roll_left"){
+      if (order_data["request"] == "roll_left")
+      {
         roll_correct = roll_correct - 1;
         save_cofig();
       }
-      if(order_data["request"] == "roll_right"){
+      if (order_data["request"] == "roll_right")
+      {
         roll_correct = roll_correct + 1;
         save_cofig();
       }
-      if(order_data["request"] == "pitch_up"){
+      if (order_data["request"] == "pitch_up")
+      {
         pitch_correct = pitch_correct + 1;
         save_cofig();
       }
-      if(order_data["request"] == "pitch_down"){
+      if (order_data["request"] == "pitch_down")
+      {
         pitch_correct = pitch_correct - 1;
         save_cofig();
       }
-      if(order_data["request"] == "kp_down"){
+      if (order_data["request"] == "kp_down")
+      {
         kp = kp - 0.01;
         rollPowerPID.SetTunings(kp, ki, kd);
         save_cofig();
       }
-      if(order_data["request"] == "kp_up"){
+      if (order_data["request"] == "kp_up")
+      {
         kp = kp + 0.01;
         rollPowerPID.SetTunings(kp, ki, kd);
         save_cofig();
       }
-      if(order_data["request"] == "ki_down"){
+      if (order_data["request"] == "ki_down")
+      {
         ki = ki - 0.01;
         rollPowerPID.SetTunings(kp, ki, kd);
         save_cofig();
       }
-      if(order_data["request"] == "ki_up"){
+      if (order_data["request"] == "ki_up")
+      {
         ki = ki + 0.01;
         rollPowerPID.SetTunings(kp, ki, kd);
         save_cofig();
       }
-      if(order_data["request"] == "kd_down"){
+      if (order_data["request"] == "kd_down")
+      {
         kd = kd - 0.01;
         rollPowerPID.SetTunings(kp, ki, kd);
         save_cofig();
       }
-      if(order_data["request"] == "kd_up"){
+      if (order_data["request"] == "kd_up")
+      {
         kd = kd + 0.01;
         rollPowerPID.SetTunings(kp, ki, kd);
         save_cofig();
@@ -191,28 +216,35 @@ void OnDataReceived(uint8_t *macAddr, uint8_t *data, uint8_t dataLength) {
   }
 }
 
-bool areMacAddressesEqual(const uint8_t mac1[6], const uint8_t mac2[6]) {
-    for (int i = 0; i < 6; i++) {
-        if (mac1[i] != mac2[i]) {
-            return false;
-        }
+bool areMacAddressesEqual(const uint8_t mac1[6], const uint8_t mac2[6])
+{
+  for (int i = 0; i < 6; i++)
+  {
+    if (mac1[i] != mac2[i])
+    {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
-void setup() {
-  Serial.begin(115200);   // 初始化串口
-  while (!Serial);   
-  while (!SPIFFS.begin());
+void setup()
+{
+  Serial.begin(115200); // 初始化串口
+  while (!Serial)
+    ;
+  while (!SPIFFS.begin())
+    ;
   Wire.begin();
   Wire.setClock(400000);
 
   delay(1000);
 
-  if(SPIFFS.exists(system_info_file)){            // 判断配置文件是否存在
+  if (SPIFFS.exists(system_info_file))
+  { // 判断配置文件是否存在
     Serial.println("SYSTEM INFO Loading!");
     DynamicJsonDocument system_info(512);
-    File file = SPIFFS.open(system_info_file,"r");   // 打开配置文件
+    File file = SPIFFS.open(system_info_file, "r"); // 打开配置文件
     deserializeJson(system_info, file);
     String system_info_str;
     serializeJson(system_info, system_info_str);
@@ -225,7 +257,9 @@ void setup() {
     yaw_correct = system_info["yaw_correct"];
     roll_correct = system_info["roll_correct"];
     pitch_correct = system_info["pitch_correct"];
-  }else{
+  }
+  else
+  {
     Serial.println("SYSTEM INFO Create!");
     DynamicJsonDocument system_info(512);
     kp = 1.6;
@@ -245,23 +279,26 @@ void setup() {
     String system_info_str;
     serializeJson(system_info, system_info_str);
     Serial.println(system_info_str);
-    File file = SPIFFS.open(system_info_file,"w");
+    File file = SPIFFS.open(system_info_file, "w");
     serializeJson(system_info, file);
     file.close();
   }
 
   WiFi.mode(WIFI_STA);
 
-  if (esp_now_init() == 0) {
+  if (esp_now_init() == 0)
+  {
     Serial.println("ESPNow Init Success");
-  }else{
+  }
+  else
+  {
     Serial.println("ESPNow Init Failed");
     // ESP-NOW 初始化失败重启系统
     ESP.restart();
   }
 
   // 设置 接收机为主控模式
-  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER); 
+  esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);
   esp_now_register_recv_cb(OnDataReceived);
 
   devStatus = mpu.dmpInitialize();
@@ -270,96 +307,101 @@ void setup() {
   mpu.setZGyroOffset(-85);
   mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
   // make sure it worked (returns 0 if so)
-  if (devStatus == 0) {
-      //Calibration Time: generate offsets and calibrate our MPU6050
-      mpu.CalibrateAccel(6);
-      mpu.CalibrateGyro(6);
-      mpu.PrintActiveOffsets();
-      // turn on the DMP, now that it's ready
-      Serial.println(F("Enabling DMP..."));
-      mpu.setDMPEnabled(true);
+  if (devStatus == 0)
+  {
+    // Calibration Time: generate offsets and calibrate our MPU6050
+    mpu.CalibrateAccel(6);
+    mpu.CalibrateGyro(6);
+    mpu.PrintActiveOffsets();
+    // turn on the DMP, now that it's ready
+    Serial.println(F("Enabling DMP..."));
+    mpu.setDMPEnabled(true);
 
-      //enable Arduino interrupt detection
-      Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-      Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-      Serial.println(F(")..."));
-      mpuIntStatus = mpu.getIntStatus();
+    // enable Arduino interrupt detection
+    Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
+    Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
+    Serial.println(F(")..."));
+    mpuIntStatus = mpu.getIntStatus();
 
-      // set our DMP Ready flag so the main loop() function knows it's okay to use it
-      Serial.println(F("DMP ready! Waiting for first interrupt..."));
-      dmpReady = true;
-      // get expected DMP packet size for later comparison
-      packetSize = mpu.dmpGetFIFOPacketSize();
-    } else {
-      // ERROR!
-      // 1 = initial memory load failed
-      // 2 = DMP configuration updates failed
-      // (if it's going to break, usually the code will be 1)
-      Serial.print(F("DMP Initialization failed (code "));
-      Serial.print(devStatus);
-      Serial.println(F(")"));
+    // set our DMP Ready flag so the main loop() function knows it's okay to use it
+    Serial.println(F("DMP ready! Waiting for first interrupt..."));
+    dmpReady = true;
+    // get expected DMP packet size for later comparison
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+  else
+  {
+    // ERROR!
+    // 1 = initial memory load failed
+    // 2 = DMP configuration updates failed
+    // (if it's going to break, usually the code will be 1)
+    Serial.print(F("DMP Initialization failed (code "));
+    Serial.print(devStatus);
+    Serial.println(F(")"));
   }
 
-  engine_right.attach(14,500,2000);
-  engine_left.attach(12,500,2000); 
+  engine_right.attach(14, 500, 2000);
+  engine_left.attach(12, 500, 2000);
 
   servo_yaw.attach(0);
   servo_pitch.attach(13);
   servo_roll.attach(15);
-  
+
   servo_roll.write(90);
   servo_pitch.write(90);
   servo_yaw.write(90);
 
   rollPowerPID.SetOutputLimits(-100, 100);
-  rollPowerPID.SetMode(AUTOMATIC);  
+  rollPowerPID.SetMode(AUTOMATIC);
 
   analogWrite(2, LOW);
-
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-  if(dmpReady){
+  if (dmpReady)
+  {
     mpu.dmpGetCurrentFIFOPacket(fifoBuffer);
     // 获取 YAW PITCH ROLL 硬解数据
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
 
-    mpu_yaw = ypr[0] * 180/M_PI;
-    mpu_pitch = ypr[1] * 180/M_PI;
-    mpu_roll  = -ypr[2] * 180/M_PI;
+    mpu_yaw = ypr[0] * 180 / M_PI;
+    mpu_pitch = ypr[1] * 180 / M_PI;
+    mpu_roll = -ypr[2] * 180 / M_PI;
 
-    //Serial.print("YAW : ");
-    //Serial.print(mpu_yaw);
-    //Serial.print("; PITCH : ");
-    //Serial.print(mpu_pitch);
-    //Serial.print("; ROLL : ");
-    //Serial.print(mpu_roll);
-    //Serial.print("; RC ROLL : ");
-    //Serial.print(roll);
-    //Serial.print("; FC : ");
-    //Serial.print(flight_control);
-    //Serial.print("; ");
-      
+    // Serial.print("YAW : ");
+    // Serial.print(mpu_yaw);
+    // Serial.print("; PITCH : ");
+    // Serial.print(mpu_pitch);
+    // Serial.print("; ROLL : ");
+    // Serial.print(mpu_roll);
+    // Serial.print("; RC ROLL : ");
+    // Serial.print(roll);
+    // Serial.print("; FC : ");
+    // Serial.print(flight_control);
+    // Serial.print("; ");
+
     // 获取姿态数据
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetAccel(&aa, fifoBuffer);
     mpu.dmpGetGravity(&gravity, &q);
     mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-    //plane_order["ax"] = aaReal.x;
-    //plane_order["ay"] = aaReal.y;
-    //plane_order["az"] = aaReal.z;
+    // plane_order["ax"] = aaReal.x;
+    // plane_order["ay"] = aaReal.y;
+    // plane_order["az"] = aaReal.z;
 
     mpu.dmpGetQuaternion(&q, fifoBuffer);
     mpu.dmpGetGravity(&gravity, &q);
-    //plane_order["gx"] = gravity.x;
-    //plane_order["gy"] = gravity.y;
-    //plane_order["gz"] = gravity.z; 
+    // plane_order["gx"] = gravity.x;
+    // plane_order["gy"] = gravity.y;
+    // plane_order["gz"] = gravity.z;
   }
 
-  if(areMacAddressesEqual(rc_address,broadcast_address)){
+  if (areMacAddressesEqual(rc_address, broadcast_address))
+  {
     DynamicJsonDocument plane_order(512);
     plane_order["request"] = "matching_codes";
     plane_order["from"] = "plane";
@@ -368,53 +410,69 @@ void loop() {
     char order_data[plane_order_str.length() + 1];
     strcpy(order_data, plane_order_str.c_str());
     esp_now_send(rc_address, (uint8_t *)&order_data, sizeof(order_data));
-    //Serial.println(plane_order_str);
+    // Serial.println(plane_order_str);
     delay(1000);
-  }else{
-    
+  }
+  else
+  {
+
     int power_right = 0;
     int power_left = 0;
-    if(flight_control){
-      if(power > 0){
-        if(roll_power_out>0){
+    if (flight_control)
+    {
+      if (power > 0)
+      {
+        if (roll_power_out > 0)
+        {
           power_right = power - twin_engine_power_correct;
           power_left = power + roll_power_out + twin_engine_power_correct;
-        }else{
+        }
+        else
+        {
           power_right = power - roll_power_out - twin_engine_power_correct;
           power_left = power + twin_engine_power_correct;
         }
-      }else{
+      }
+      else
+      {
         power_right = 0;
         power_left = 0;
       }
       rollPowerPID.Compute();
-      //Serial.print(" roll_power_out : ");
-      //Serial.print(roll_power_out);
-      //Serial.print("; ");
-    }else{
-      if(power > 0){
-        if(diff_power > 0){
-          power_right = power  - twin_engine_power_correct;
+      // Serial.print(" roll_power_out : ");
+      // Serial.print(roll_power_out);
+      // Serial.print("; ");
+    }
+    else
+    {
+      if (power > 0)
+      {
+        if (diff_power > 0)
+        {
+          power_right = power - twin_engine_power_correct;
           power_left = power + diff_power + twin_engine_power_correct;
-        }else{
+        }
+        else
+        {
           power_right = power - diff_power - twin_engine_power_correct;
-          power_left = power  + twin_engine_power_correct;
+          power_left = power + twin_engine_power_correct;
         }
       }
     }
-    //Serial.print("POWER RIGHT : ");
-    //Serial.print(power_right);
-    //Serial.print("; POWER LEFT : ");
-    //Serial.print(power_left);
-    //Serial.println(";");
-    engine_right.write(1000 + int(power_right*1000/100));
-    engine_left.write(1000 + int(power_left*1000/100));
+    // Serial.print("POWER RIGHT : ");
+    // Serial.print(power_right);
+    // Serial.print("; POWER LEFT : ");
+    // Serial.print(power_left);
+    // Serial.println(";");
+    engine_right.write(1000 + int(power_right * 1000 / 100));
+    engine_left.write(1000 + int(power_left * 1000 / 100));
 
     servo_yaw.write(90 + yaw + yaw_correct);
     servo_pitch.write(90 - pitch + pitch_correct);
     servo_roll.write(90 + roll + roll_correct);
 
-    if(millis() - t > 100){
+    if (millis() - t > 100)
+    {
       t = millis();
       DynamicJsonDocument plane_order(512);
       plane_order["request"] = "plane_state";
@@ -433,9 +491,6 @@ void loop() {
       strcpy(order_data, plane_order_str.c_str());
       esp_now_send(rc_address, (uint8_t *)&order_data, sizeof(order_data));
     }
-
-    
-
 
     delay(10);
   }
